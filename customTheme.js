46f7240aa59177349168dab29a078360da9d2d3e
ClockWize171 <jupiterminhtin998@gmail.com>
2023-02-21 11:40:19 +0630

    initial commit

---
+++ customTheme.js
@@ -0,0 +1,34 @@
+
+
+export const colors = {
+  fonts: {
+    heading: `'Lato', sans-serif`,
+    body: `'Lato', sans-serif`,
+  },
+  primary: {
+    10: '#E5B8F4',
+    20: '#C147E9',
+    30: '#810CA8',
+  },
+  secondary: {
+    10: '#40E0D0',
+    20: '#0AB0A0',
+    30: '#008678',
+  },
+  tertiary: {
+    10: '#72B5B7',
+    20: '#5D9698',
+    30: '#4B6C6D',
+  },
+  accent: {
+    10: '#633A82',
+    20: '#4A2A63',
+    30: '#38214A',
+  },
+  myPurple: {
+    10: '#EDE7EF',
+    20: '#9869A8',
+    30: '#66287B',
+    40: '#2D033B',
+  }
+}
---
+++ src/pages/dall-e/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DallE = () => {
+  return (
+    <div>DallE</div>
+  )
+}
+
+export default DallE
